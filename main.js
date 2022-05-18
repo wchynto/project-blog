b9efb6cd78fe51634d49a49c537b77762ea2062d
wchynto <83586819+wchynto@users.noreply.github.com>
2022-05-18 22:30:39 +0700

    membuat bebrapa view

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import express from "express";
-import ejs from "ejs";
 import morgan from "morgan";
 
 import "./config/database.js";
@@ -7,11 +6,40 @@ import "./config/database.js";
 const port = 3000;
 const app = express();
 
-app.set("view engine", ejs);
+app.set("view engine", "ejs");
 
-app.use(morgan("combined"));
+app.use(morgan("short"));
 
 //route on here
+app.get("/", (req, res) => {
+  res.render("home", {
+    name: "home",
+  });
+});
+
+app.get("/posts", (req, res) => {
+  res.render("posts", {
+    name: "posts",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    name: "about",
+  });
+});
+
+app.get("/register", (req, res) => {
+  res.render("register", {
+    name: "register",
+  });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login", {
+    name: "login",
+  });
+});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
